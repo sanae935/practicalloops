1417204fe0c195af084faf66cf07bb63401b41a4
sanae935 <s.elatmani005@gmail.com>
2024-09-24 13:58:20 -0400

    pushing index.js file

---
+++ index.js
@@ -0,0 +1,34 @@
+// Part 1: Fizz Buzz
+for(let i = 1; i <= 100; i++) {
+if ( i % 3 === 0 && i % 5 === 0) {
+    console.log("Fizz Buzz");
+} else if (i % 3 === 0) {
+    console.log("Fizz");
+
+} else if (i % 5 === 0) {
+    console.log("Buzz");
+}else {
+    console.log(i)
+}
+
+}
+
+//Part 2: Prime Time
+ function isPrime(N) {
+    if (N <= 1) false; 
+    for (let i = 2; i <= Math.sqrt(N); i++) {
+        if (N % i === 0) false;
+    }
+    return true; 
+}
+
+function NextPrime(n) {
+    let nextNum =n + 1; 
+    while (true) {
+    if (isPrime(nextNum)) {
+    console.log(nextNum); 
+    break; 
+}
+    nextNum++; 
+}
+}
